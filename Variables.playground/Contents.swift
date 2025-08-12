dd43d35285a4a77881978a7bd6bb6e9f46dcc44f
_costa <luizfelipeeoliveiraac@gmail.com>
2025-08-12 19:29:42 -0400

    feat: first lesson of learning programming language Swift

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+// let = immutabilty variables
+// var = mutabily variables
+
+// example dont mutated variables
+let myName = "Luiz"
+let lasName = "Costa"
+
+//Cannot assign to value: 'myName' is a 'let' constant
+//myName = "Luiz"
+
+
+// example mutated variables
+var names = ["Luiz", "Lucas"]
+
+names = ["João"]
+names.append("Maria")
+names.append("Kayke")
+
+
+
